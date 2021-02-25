67f6fbbbfdf5acc71c9d68feddaedd5e68332e43
Larn3xd <thelarn@yandex.ru>
2021-02-26 00:49:14 +0300

    JavaUpd

--- ktp/src/Palindrome.java
+++ ktp/src/Palindrome.java
@@ -4,14 +4,14 @@ public class Palindrome {
 		     String[] words = {"java", "Palindrome","madam","racecar","apple","kayak","song","noon"};     
 		 for (int i = 0; i < words.length; i++) {
 		     if (isPalindrome(words[i]) == true) {
-		         System.out.println(words[i] + " - Является Палиндромом");//  
+		         System.out.println(words[i] + " - Является Палиндромом");  
 		     }
-		     else System.out.println(words[i] + " - Не является Палиндромом");// 
+		     else System.out.println(words[i] + " - Не является Палиндромом"); 
 		}
 		 }
 		
 		public static boolean isPalindrome(String s) {
-		    String palindrome = ""; // 
+		    String palindrome = ""; 
 		    for (int i = s.length() - 1; i >= 0; i--) {  
 		        palindrome = palindrome + s.charAt(i);
 		    }
